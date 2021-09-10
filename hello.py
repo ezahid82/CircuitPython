e52e0e516cb01963f6dbd8d11b3f6db5eb4a86a2
Ezhar Zahid <ezahid82@ccs.local>
2021-09-10 15:10:13 -0400

    adding my first assignment code

---
+++ hello.py
@@ -0,0 +1,16 @@
+# Write your code here :-)
+import board
+import neopixel
+import time
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+
+while True:
+    print("Make it red!")
+    print("Make it red!")
+    dot.fill((0, 225, 0))
+    time.sleep(.5)
+    print("Make it Blue")
+    dot.fill((225, 0, 0))
+    time.sleep(.5)
